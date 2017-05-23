f32879dfac13493663431203702ba854b881991c
zainnadeem <zn.nadeem@gmail.com>
2017-05-22 23:08:16 -0400

    first commit

---
+++ C4QWeather/AerisAPIClient.swift
@@ -0,0 +1,9 @@
+//
+//  AerisAPIClient.swift
+//  C4QWeather
+//
+//  Created by Zain Nadeem on 5/22/17.
+//  Copyright © 2017 Zain Nadeem. All rights reserved.
+//
+
+import Foundation
---
+++ C4QWeather/ForecastTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ForecastTableViewCell.swift
+//  C4QWeather
+//
+//  Created by Zain Nadeem on 5/22/17.
+//  Copyright © 2017 Zain Nadeem. All rights reserved.
+//
+
+import UIKit
+
+class ForecastTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
