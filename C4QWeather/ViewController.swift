75c59ab10096c166affd97d56c95c854e36738bc
zainnadeem <zn.nadeem@gmail.com>
2017-05-22 20:10:44 -0400

    Initial Commit

---
+++ C4QWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  C4QWeather
+//
+//  Created by Zain Nadeem on 5/22/17.
+//  Copyright © 2017 Zain Nadeem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
